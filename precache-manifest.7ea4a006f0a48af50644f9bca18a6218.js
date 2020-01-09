7796ab3d6dcb2a4db32125e3d6312dcbb6372168
Honza Kropacek <kropacek.h@gmail.com>
2020-01-09 16:30:07 +0100

    Updates

---
+++ precache-manifest.7ea4a006f0a48af50644f9bca18a6218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5532b139339585f00ec41e8bf211aa",
+    "url": "/test-react-app/index.html"
+  },
+  {
+    "revision": "cbb2d70ffb768cda4da1",
+    "url": "/test-react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5204f9b94fffba0c24a7",
+    "url": "/test-react-app/static/js/2.5c226317.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-react-app/static/js/2.5c226317.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbb2d70ffb768cda4da1",
+    "url": "/test-react-app/static/js/main.0b1a0fc5.chunk.js"
+  },
+  {
+    "revision": "d49ff860576a295b679d",
+    "url": "/test-react-app/static/js/runtime-main.51df336e.js"
+  }
+]);
